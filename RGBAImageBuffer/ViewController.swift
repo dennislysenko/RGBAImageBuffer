6fc19b9817bd457a10fb605789d8399605162a57
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2016-04-26 19:29:21 -0400

    slightly refactor demo view controller

--- RGBAImageBuffer/ViewController.swift
+++ RGBAImageBuffer/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var imageView: UIImageView!
-    let image = UIImage(named: "sample_photo")!
-    var rgbBuffer: RGBAImageBuffer!
+    
+    var rgbaBuffer: RGBAImageBuffer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.rgbBuffer = RGBAImageBuffer(image: self.image.CGImage)
-        self.imageView.image = self.image
+        let image = UIImage(named: "sample_photo")!
+        self.imageView.image = image
+        
+        // Create the buffer: it's that easy.
+        // NB: if your image is big, you will want to do this in the background.
+        self.rgbaBuffer = RGBAImageBuffer(image: image.CGImage)
+        
+        // Make the image view color the background of our whole view when you move around on it to demonstrate that the RGBAImageBuffer works
         self.imageView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(ViewController.extractColorFromImage(_:))))
         self.imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.extractColorFromImage(_:))))
         self.imageView.userInteractionEnabled = true
@@ -25,11 +32,14 @@ class ViewController: UIViewController {
     
     func extractColorFromImage(gesture: UIGestureRecognizer) {
         let location = gesture.locationInView(self.imageView)
-        let x = Int(location.x / self.imageView.bounds.width * self.image.size.width * self.image.scale)
-        let y = Int(location.y / self.imageView.bounds.height * self.image.size.height * self.image.scale)
         
-        let color = self.rgbBuffer[x, y]!
-        self.view.backgroundColor = color
+        // rescale coordinates from the imageView's coordinate space to the raw image / rgba buffer's coordinate space
+        let x = Int(location.x / self.imageView.bounds.width * CGFloat(self.rgbaBuffer.width))
+        let y = Int(location.y / self.imageView.bounds.height * CGFloat(self.rgbaBuffer.height))
+        
+        if let color = self.rgbaBuffer[x, y] {
+            self.view.backgroundColor = color
+        }
     }
 }
 
