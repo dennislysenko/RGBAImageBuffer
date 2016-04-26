cec7b6d49f906003dfd9fcd0da26fd53d458a09c
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2016-04-26 19:24:50 -0400

    make it possible to drag on the image in the demo instead of tapping it

--- RGBAImageBuffer/ViewController.swift
+++ RGBAImageBuffer/ViewController.swift
@@ -18,11 +18,11 @@ class ViewController: UIViewController {
         
         self.rgbBuffer = RGBAImageBuffer(image: self.image.CGImage)
         self.imageView.image = self.image
-        self.imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.didTapImage(_:))))
+        self.imageView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(ViewController.didPanImage(_:))))
         self.imageView.userInteractionEnabled = true
     }
     
-    func didTapImage(gesture: UITapGestureRecognizer) {
+    func didPanImage(gesture: UIPanGestureRecognizer) {
         let location = gesture.locationInView(self.imageView)
         let x = Int(location.x / self.imageView.bounds.width * self.image.size.width * self.image.scale)
         let y = Int(location.y / self.imageView.bounds.height * self.image.size.height * self.image.scale)
